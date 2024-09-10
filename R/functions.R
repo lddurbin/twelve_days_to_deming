62bcbc91d39e55298126b25d2b01de36f16d1d16
lddurbin <l.d.durbin@gmail.com>
2024-09-11 09:51:01 +1200

    Perfecting the look and placement of the clocks. Progressing Day 1.

--- R/functions.R
+++ R/functions.R
@@ -16,8 +16,8 @@ create_clock <- function(hour, minute) {
   clock_data <- data.frame(
     x = c(0, 0),
     y = c(0, 0),
-    xend = c(0.15 * cos(hour_angle), 0.22 * cos(minute_angle)),  # Slightly reduce hand length
-    yend = c(0.15 * sin(hour_angle), 0.22 * sin(minute_angle)),
+    xend = c(0.015 * cos(hour_angle), 0.023 * cos(minute_angle)),  # Slightly reduce hand length
+    yend = c(0.015 * sin(hour_angle), 0.023 * sin(minute_angle)),
     hand = c("hour", "minute")
   )
   
@@ -25,31 +25,33 @@ create_clock <- function(hour, minute) {
   tick_angles <- seq(0, 2 * pi, length.out = 13)[-13]  # Remove the last value (full circle repetition)
   
   tick_data <- data.frame(
-    x = 0.26 * cos(tick_angles),
-    y = 0.26 * sin(tick_angles),
-    xend = 0.22 * cos(tick_angles),  # Short inward-facing dashes
-    yend = 0.22 * sin(tick_angles)
+    x = 0.028 * cos(tick_angles),
+    y = 0.028 * sin(tick_angles),
+    xend = 0.02 * cos(tick_angles),  # Short inward-facing dashes
+    yend = 0.02 * sin(tick_angles)
   )
   
   # Create ggplot clock
   ggplot() + 
     # Circle for clock face (smaller size)
-    geom_point(aes(x = 0, y = 0), size = 40, shape = 1) +  # Adjust circle size
+    geom_point(aes(x = 0, y = 0), size = 150, shape = 1) +  # Adjust circle size
     
     # Small inner circle at the center of the clock
-    geom_point(aes(x = 0, y = 0), size = 4, shape = 1) +  # Smaller inner circle for hands attachment
+    geom_point(aes(x = 0, y = 0), size = 15, shape = 1) +  # Smaller inner circle for hands attachment
     
     # Clock ticks as inward-facing dashes
-    geom_segment(data = tick_data, aes(x = x, y = y, xend = xend, yend = yend), size = 0.5) + 
+    geom_segment(data = tick_data, aes(x = x, y = y, xend = xend, yend = yend), size = 0.4) + 
     
     # Hour and minute hands (slightly shorter)
     geom_segment(data = clock_data, aes(x = x, y = y, xend = xend, yend = yend, colour = hand), size = 1.5) +
     scale_colour_manual(values = c("black", "black")) +
-    
-    # Styling
-    xlim(-1, 1) +
-    ylim(-1, 1) +
+    coord_fixed(xlim = c(-0.1, 0.1), ylim = c(-0.1, 0.1), expand = FALSE) + # Adjust to ensure clock fills the space
     theme_void() + 
-    theme(legend.position = "none") +
+    theme(
+      legend.position = "none",
+      plot.margin = unit(c(-0.01, -0.01, -0.01, -0.01), "cm"),
+      panel.spacing = unit(0, "cm"),
+      plot.background = element_rect(fill = "transparent", colour = NA)
+      ) +
     coord_fixed()
 }
